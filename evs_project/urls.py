7e1f49f6799777567888e5d98614d22fabc778a0
Nonso <ebeagujohn5@gmail.com>
2026-07-22 14:54:44 +0100

    Serve media files in production without relying on DEBUG

--- evs_project/urls.py
+++ evs_project/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('healthz/', views.healthz, name='healthz'),
     path('', views.onboarding, name='onboarding'),
     path('voters/', include('voters.urls')),
     path('candidates/', include('candidates.urls')),
@@ -14,4 +13,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
