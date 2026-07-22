ddc1d04245a135211a3604f559358edfd2d0e827
Nonso <ebeagujohn5@gmail.com>
2026-07-22 16:21:29 +0100

    Add vote confirmation modal and post-vote thank-you page

--- voting/urls.py
+++ voting/urls.py
@@ -6,8 +6,8 @@ app_name = "voting"
 urlpatterns = [
     path("login/", views.voting_login, name="voting_login"),
     path("vote/", views.voting_cast_vote, name="voting_cast_vote"),
+    path("thank-you/", views.voting_thank_you, name="voting_thank_you"),
     path("results/", views.voting_results, name="voting_results"),
     path("election-settings/", views.election_settings, name="voting_election_settings"),
     path("logout/", views.voting_logout, name="voting_logout"),
-    path("thank-you/", views.voting_thank_you, name="voting_thank_you"),
 ]
